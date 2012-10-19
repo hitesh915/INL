3704c1c8debe84ac033b2957f5e6d9f2eb07b343
Joan T. Matamalas Llodrà <jtmatamalas@gmail.com>
2012-10-19 21:01:18 +0200

    PCA implemented
    Implementació de PCA completada. Falta revisar si els valors retornats
    per la funció són suficients per realitzar un analisi de els dades.

--- work1/main.m
+++ work1/main.m
@@ -1,3 +1,4 @@
-matrix = parser_arff('data/vehicle.arff');
+matrix = parser_arff('data/breast-w.arff');
 matrix = standarizerS(matrix);
 [membership,centroids, sumD] = k_means(matrix,3,42);
+[dataAfterPCA eVectors eValues] = pca(matrix,1);
---
+++ work1/pca.m
@@ -0,0 +1,26 @@
+function [ outData, eVectors, eValues] = pca( dataMatrix, eValueThreshold)
+%PCA - Principal Component Analysis implementation
+    
+    %Calculate the covariance matrix
+    covMatrix = cov(dataMatrix);
+    
+    %Calculate the eigenvectors and eigenvalues of the covariance matrix
+    [eVectors, eValues] = eig(covMatrix);
+    
+    %Choose components
+    %order the eValues and eVectors in decend order
+    eValues = flipud(diag(eValues));
+    eVectors = fliplr(eVectors);
+    
+    %construt a new feature vector based on the eValueThreshold
+    featureVector = eVectors(:, eValues >= eValueThreshold);
+    
+    %Derive the new data set
+    transformedData = featureVector.' * dataMatrix.';
+    
+    %Reconstruct the old data back
+    rowData = featureVector * transformedData;
+    outData = rowData.';
+    
+end
+
