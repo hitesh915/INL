b99fc8e69ed8bee0571036db290379f5e33bb377
Marc Oliu <gaspercat@gmail.com>
2012-10-27 15:53:09 +0200

    Now main.m performs the k-means for K=2:5 and for each value selects the best seed between 1:25

--- work1/main.m
+++ work1/main.m
@@ -2,14 +2,38 @@ matrix = parser_arff('data/breast-w.arff');
 matrix1 = standarizerS(matrix);
 matrix2 = standarizer(matrix);
 
-%Compute the k clusters with k-means
-[output, centroids, sum] = k_means(matrix1, 2, 7);
+% -- K-Means for different K values
+% ------------------------------------------------------
 
-%Use the MDS algorithm to reduce the dimensionality
-dimReduced = mds(matrix1,3);
+for k = 2:5
+    % Initialize best seed result value
+    best_inertia = nan;
+    best_output = nan;
+    best_centroids = nan;
+    
+    % Calculate with different seeds and select best
+    for seed = 1:25
+        %Compute the k clusters with k-means
+        [output, centroids, inertias] = k_means(matrix1, k, seed);
+        
+        %Select this result if lower inertia than the previous ones
+        best_inertia = min(best_inertia, sum(inertias))
+        if best_inertia == sum(inertias)
+            best_output = output;
+            best_centroids = centroids;
+        end
+    end
+    
+    %Use the MDS algorithm to reduce the dimensionality
+    dimReduced = mds(matrix1,3);
 
-%Show a scatter plot in 3D of the clustered data.
-scatter3(dimReduced(:,1),dimReduced(:,2),dimReduced(:,3), 20, output, 'filled');
+    %Show a scatter plot in 3D of the clustered data with the best seed
+    subplot(2,2,k-1);
+    scatter3(dimReduced(:,1),dimReduced(:,2),dimReduced(:,3), 20, best_output, 'filled');
+end
+
+% -- PCA
+% ------------------------------------------------------
 
 %Compute the PCA transform
 %[dataAfterPCA eVectors eValues] = pca(matrix,1);
