4522cc8219c2fad5a5b102971cdc23522ccd5292
Marc Oliu <gaspercat@gmail.com>
2012-11-17 20:04:13 +0100

    Added main.m code for the second part (weighted and selected kNN calls)

--- work2/main.m
+++ work2/main.m
@@ -1,4 +1,4 @@
-dataset = 'waveform';
+dataset = 'pen-based';
 
 K = [1,3,5,7,9,11,13];
 R = 1:3;
@@ -7,13 +7,17 @@ meansAccuracyList = [];
 stdAccuracyList = [];
 SEMList = [];
 
-%Get the data from files
+% Get the data from files
 data = [];
 for fold = 1:10
    [train, test] = parser_nfold(dataset, fold);
    data = [data;{train,test}];
 end
 
+% STEP 1: TEST DIFFERENT K AND R VALUES
+% --------------------------------------------------------------
+
+% Test different K and R values
 for r = R
     for k = K
         accuracies = zeros(1, 10);
@@ -34,5 +38,46 @@ for r = R
         SEMList = [SEMList, accuracySEM];
     end
 end
-
 ploting(K, meansAccuracyList, SEMList);
+
+
+% STEP 2: APPLY WEIGHTED AND SELECTED KNN ALGORITHMS
+% --------------------------------------------------------------
+
+% Set K and R values to use
+k = 1;
+r = 1;
+
+% Initialize accuracy vectors
+wAccuracies = zeros(1, 10);
+sAccuracies = zeros(1, 10);
+
+% Classify folds with the algorithms
+for fold = 1:10
+    train = data{fold,1};
+    test = data{fold,2};
+    wAccuracies(fold) = cbr(train, test, k, r, 2);
+    sAccuracies(fold) = cbr(train, test, k, r, 3);
+end
+
+% Calculate mean accuracy for the weighted kNN algorithm
+wAccuracyMean = mean(wAccuracies);
+wAccuracySTD = std(wAccuracies);
+wAcuracySEM = wAccuracySTD/sqrt(size(K,2));
+
+% Show results for weighted kNN
+fprintf('Weighted kNN:');
+fprintf(strcat('Mean accuracy:\t\t', num2str(wAccuracyMean), '\n'));
+fprintf(strcat('Accuracy standard dev.:\t', num2str(wAccuracySTD), '\n'));
+fprintf(strcat('Standard Error of Mean:\t', num2str(wAccuracySEM), '\n'));
+
+% Calculate mean accuracy for the selected kNN algorithm
+sAccuracyMean = mean(sAccuracies);
+sAccuracySTD = std(sAccuracies);
+sAcuracySEM = sAccuracySTD/sqrt(size(K,2));
+
+% Show results for segmented kNN
+fprintf('Weighted kNN:');
+fprintf(strcat('Mean accuracy:\t\t', num2str(sAccuracyMean), '\n'));
+fprintf(strcat('Accuracy standard dev.:\t', num2str(sAccuracySTD), '\n'));
+fprintf(strcat('Standard Error of Mean:\t', num2str(sAccuracySEM), '\n'));
