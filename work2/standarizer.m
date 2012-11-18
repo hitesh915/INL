39715122c75d7ea4c68f430c659c1a57329c5a83
Marc Oliu <gaspercat@gmail.com>
2012-11-18 19:11:32 +0100

    Commented the rest of the functions of the project

--- work2/standarizer.m
+++ work2/standarizer.m
@@ -1,10 +1,17 @@
 function [ matrixZ, meanC, stdDevC ] = standarizer( matrix )
-%STANDARIZERS Returns a standarized version of the data with the missing
-%values substituted with the mean of the attribute
+    %STANDARIZERS Returns a standarized version of the data with the missing
+    %  values substituted with the mean of the attribute
+    %
+    %  INPUTS:
+    %    matrix = Matrix with the data to standarize
+    %
+    %  OUTPUTS:
+    %    matrixZ = Original matrix standarized
+    %    meanC = Vector with the mean of each column
+    %    stdDevC = Vector with the standard deviation of each column
 
-    %Compute the mean of each column (NaNs no compute)
+    %Compute the mean & standard deviation of each column (NaNs no compute)
     meanC = nanmean(matrix);
-    
     stdDevC = nanstd(matrix);
     
     %Logical matrix with ones in the place of NaNs and 0 the rest
--- work2/weightedKNN.m
+++ work2/weightedKNN.m
@@ -1,6 +1,19 @@
 function [ kPredictors, kWeightedDistances ] = weightedKNN(TrainMatrix, current_instance, K, r, weights)
-%kNN Summary of this function goes here
-%   Detailed explanation goes here
+    %kNN Finds the K nearest predictors for the current instance by using
+    %  the r-th minkowski distance, and modifying the distance of each
+    %  dimension according to its weight.
+    %
+    %  INPUTS:
+    %    TrainMatrix = Matrix with all the predictors
+    %    current_instance = Instance to classify
+    %    K = Number of neighbors to retrieve
+    %    r = Type of minkowski distance to use
+    %    weights = Vector with the weight of each dimension
+    %
+    %  OUTPUTS:
+    %    kPredictors = Vector with the K nearest predictors
+    %    kWeightedDistances = Vector with the weighted distances of the K
+    %    returned predictors
 
     %Calc the minkowski distance from the current_instance to the
     %predictors taking into account the weights of each attribute.
