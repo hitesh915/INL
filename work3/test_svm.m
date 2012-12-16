240df3526709660e2a552953a8a7ddfd006c8de2
Marc Oliu <gaspercat@gmail.com>
2012-12-16 13:06:38 +0100

    Fixed the RBF classifier and made SVM less sensitive to C

--- work3/test_svm.m
+++ work3/test_svm.m
@@ -10,22 +10,15 @@ function [ predicted ] = test_svm( svmStruct, test )
     if strcmp(svmStruct.kernel,'linear')
         predicted = sign(test*w+b);
     else
-%         kernel = trainData*trainData' / sigma^2;
-%         d = diag(kernel);
-%         kernel = kernel - ones(n,1)*d'/2;
-%         kernel = kernel - d*ones(1,n)/2;
-%         kernel = exp(kernel);
-%         
-%         predicted = sign(kernel*((svmStruct.alpha).*svmStruct.labels)+b);
+        kernels = rbfKernel(svmStruct.sv_points,test, svmStruct.sigma);
 
         predicted = zeros(size(test,1),1);
         for n = 1:size(test,1)
-            kernels = rbfKernel(svmStruct.sv_points,test(n,:), svmStruct.sigma);
             result = 0;
             for i = 1:size(svmStruct.sv_points,1)
-                result = result + (svmStruct.sv_alphas(i) * svmStruct.sv_labels(i) * kernels(i) + b);
+                result = result + (svmStruct.sv_alphas(i) * svmStruct.sv_labels(i) * kernels(i,n));
             end
-            predicted(n) = sign(result);
+            predicted(n) = sign(result + b);
         end
     end
 end
--- work3/train_svm.m
+++ work3/train_svm.m
@@ -14,7 +14,7 @@ function [model] = train_svm(labels, data, C, sigma)
     
     % Define linear/RBF kernel
     if nargin < 4
-        kernel = data*data';
+        kernel = linearKernel(data, data);
     else
         kernel = rbfKernel(data, data, sigma);
 %         kernel = data*data' / sigma^2;
@@ -31,7 +31,7 @@ function [model] = train_svm(labels, data, C, sigma)
     %Dual form (using quadratic programming via CVX)
     cvx_clear
     cvx_begin
-        variables alpha(n)
+        variable alpha(n)
         maximize(sum(alpha) -  0.5*quad_form(labels.*alpha,kernel))
         subject to
            alpha>=0
@@ -40,15 +40,30 @@ function [model] = train_svm(labels, data, C, sigma)
     cvx_end
  
     epsilon = 0.0001;
-    sv_filter = (alpha > epsilon & alpha < (C - epsilon));
     svii = find( alpha > epsilon & alpha < (C - epsilon));
     
     % Obtain model parameters
-    model_b = (1/length(svii))*sum(labels(svii) - kernel(svii,:)*alpha.*labels(svii));
-    model_w = data'*(alpha.*labels);
+    %model_b = (1/length(svii))*sum(labels(svii) - kernel(svii,:)*alpha.*labels(svii));
+    %model_w = data'*(alpha.*labels);
 
-    % Obtain support vectors
-    sv = data(alpha > epsilon & alpha < (C - epsilon), :);
+    % Obtain support vectors data
+    sv = data(svii, :);
+    sv_alphas = alpha(svii,:);
+    sv_labels = labels(svii,:);
+    sv_points = data(svii,:);
+    
+    % Get hyperplane vector
+    model_w = sv_points'*(sv_alphas.*sv_labels);
+    
+    % Get hyperplane offset
+    model_b = 0;
+    for i = 1:size(sv_points,1)
+        model_b = model_b + sv_labels(i);
+        for j = 1:size(data,1)
+            model_b = model_b - alpha(j) * labels(j) * kernel(svii(i), j);
+        end
+    end
+    model_b = model_b / size(sv_points,1);
     
     % Create model structure
     model = struct('kernel', 'linear', 'w', model_w, 'b', model_b);
